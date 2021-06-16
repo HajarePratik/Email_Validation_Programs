add48aaddd128081c24f72ed0b389239349c6415
HajarePratik <pratikhajare09@gmail.com>
2021-06-16 18:05:52 +0530

    Validated @xyz thatformat mandatory for Email ID Using Testcase

--- main/java/com/bridgelabz/email_programs/EmailProgram.java
+++ main/java/com/bridgelabz/email_programs/EmailProgram.java
@@ -1,16 +1,14 @@
 package com.bridgelabz.email_programs;
-
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 
 public class EmailProgram 
 {
 
-	// this method for test case 
+	// this method for uc2 test case 
 	public boolean validedemail(String email) 
 	{
-		return Pattern.matches("([a-zA-Z0-9.]+)", email);
+		return Pattern.matches("^@[a-zA-Z0-9.]+$", email);
 	}
 			
 
--- test/java/com/bridgelabz/email_programs/EmailProgramTest.java
+++ test/java/com/bridgelabz/email_programs/EmailProgramTest.java
@@ -8,13 +8,15 @@ import org.junit.Test;
 public class EmailProgramTest {
 
 	
-	// UC1 To check email is valid or not
+	// UC2 To check email is valid or not
 	@Test
 	public void EmailValidationItShouldReturnValidOrNot()
 	{
 		EmailProgram email = new EmailProgram();
-		assertTrue(email.validedemail("abc.123"));
-		assertTrue(email.validedemail("pratik.0907"));
+		assertTrue(email.validedemail("@bridgelabz."));
+		assertTrue(email.validedemail("@bridgelabz"));
+		assertTrue(email.validedemail("@bridgelabz123"));
+		assertTrue(email.validedemail("@123bridgelabz"));
 	}
 	
 	
