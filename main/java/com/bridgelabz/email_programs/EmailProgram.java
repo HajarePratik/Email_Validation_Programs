9d2775dcc54fb0026f79a22bc41d0a20adff0919
HajarePratik <pratikhajare09@gmail.com>
2021-06-16 19:17:09 +0530

    Validated and Adding Option format for Email ID and Merge All UC Using Testcase

--- main/java/com/bridgelabz/email_programs/EmailProgram.java
+++ main/java/com/bridgelabz/email_programs/EmailProgram.java
@@ -5,10 +5,10 @@ import java.util.regex.Pattern;
 public class EmailProgram 
 {
 
-	// this method for uc4 test case 
+	// this method for uc5 test case 
 	public boolean validedemail(String email) 
 	{
-		return Pattern.matches("^([a-zA-Z0-9.]+)([0-9a-zA-Z]{0,1})$", email);
+		return Pattern.matches("^([a-zA-Z0-9.])+([0-9a-zA-Z]{0,1}+@([a-zA-Z0-9.])+[a-zA-Z]{2,3})+([a-z]{0,2}){0,1}$", email);
 	}
 			
 
--- test/java/com/bridgelabz/email_programs/EmailProgramTest.java
+++ test/java/com/bridgelabz/email_programs/EmailProgramTest.java
@@ -8,14 +8,14 @@ import org.junit.Test;
 public class EmailProgramTest {
 
 	
-	// UC4 To check email is valid or not
+	// UC5 To check email is valid or not
 	@Test
 	public void EmailValidationItShouldReturnValidOrNot()
 	{
 		EmailProgram email = new EmailProgram();
-		assertTrue(email.validedemail("abc.xyz"));
-		assertTrue(email.validedemail("pratik.in"));
-		assertTrue(email.validedemail("a.com"));
+		assertTrue(email.validedemail("abc.xyz@bridgelabz.co.in"));
+		assertTrue(email.validedemail("pratik.0907@bridgelabz.co"));
+		assertTrue(email.validedemail("pratik@bridgelabz.co.in"));
 	}
 	
 	
